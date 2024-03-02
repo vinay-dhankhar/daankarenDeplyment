b81c225656c473524b40a21468d6835003698412
Bhavya Arora <bhavya56arora11@gmail.com>
2024-03-02 14:14:35 +0530

    Created the donation card structure

---
+++ frontend/src/components/CardZone.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Card from "./CardElement";
+
+export default function App() {
+  return (
+    <Card
+      img="https://st.depositphotos.com/1265075/3165/i/450/depositphotos_31657517-stock-photo-donate-web-concept.jpg"
+      title="Donate to random charity"
+    />
+  );
+}
