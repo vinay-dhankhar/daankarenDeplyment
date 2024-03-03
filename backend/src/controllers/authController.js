86ac4ba5e93c4d7c7364ebee75aa460d7616168f
KhushmeetChugh <133222681+KhushmeetChugh@users.noreply.github.com>
2024-03-03 06:38:49 +0530

    hasing done

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,21 +1,25 @@
 const User= require('../models/userModel');
+const bcrypt=require('bcrypt');
 const signup=async(req,res)=>{
     try{
         const {username,password,email}=req.body;
         console.log("username"+username+"pasw"+password+"email"+email);
         const existingUser = await User.findOne({ email });
         if(existingUser){
-            res.json(401).json({message:"User already Exists"});
+            res.status(401).json({message:"User already Exists"});
             return;
         }
+        else{
+        const hashedPassword = await bcrypt.hash(password, 10);
         const newUser=new User({
             username,
-            password,
+            password:hashedPassword,
             email,
             role:"user",
         })
         await newUser.save();
         res.status(200).json({message:"Signup Success"});
+    }
     } catch(error){
         res.status(500).json({message :"Signup failed"});
     }
