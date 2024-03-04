01dd35b006fb04d4d1a20d4e7450837ae3d94a5f
SnehRathi223 <snehrathi223@gmail.com>
2024-03-04 15:18:20 +0530

    navbar set

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -1,9 +1,5 @@
-import React from 'react'
-<<<<<<< Updated upstream
-=======
+import React from 'react';
 import Carousel from './Carousel';
->>>>>>> Stashed changes
-
 function HomePage() {
   return (
     <>
