f3cbe157934bb1ddabb7ed398964313c9777e737
Raghav Gupta <rg9542109@gmail.com>
2024-03-15 19:05:51 +0530

    Changes to homepage

--- frontend/src/components/HomepageDataSection.js
+++ frontend/src/components/HomepageDataSection.js
@@ -1,4 +1,7 @@
 import React from "react";
+import { FaHeart } from "react-icons/fa";
+import { FaMoneyBill1Wave } from "react-icons/fa6";
+import { FaHandHoldingHeart } from "react-icons/fa";
 
 // css written in homepage.css file
 const DataSection = () => {
@@ -7,9 +10,24 @@ const DataSection = () => {
             <div className="data-section">
                 <div className='data'>
                     <div className='data-cards-container'>
-                        <div className='data-cards'>Lives Improved</div>
-                        <div className='data-cards'>Fund Collected For Campaingns</div>
-                        <div className='data-cards'>Donations Accepted This Month</div>
+                        <div className='data-cards'>
+                            <div className="icon-container">
+                            <FaHeart style={{color: 'white', fontSize: '4rem'}}/>
+                            </div>
+                            <span>Lives Improved</span>
+                        </div>
+                        <div className='data-cards'>
+                            <div className="icon-container">
+                            <FaMoneyBill1Wave style={{color: 'white', fontSize: '4rem'}}/>
+                            </div>
+                            <span>Funds Collected for Campaigns</span>
+                        </div>
+                        <div className='data-cards'>
+                            <div className="icon-container">
+                            <FaHandHoldingHeart style={{color: 'white', fontSize: '4rem'}}/>
+                            </div>
+                            <span>Donations Accepted this Month</span>
+                        </div>
                     </div>
                 </div>
             </div>
--- frontend/src/components/Reviews-Section.js
+++ frontend/src/components/Reviews-Section.js
@@ -1,53 +1,108 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import ReviewCard from "./ReviewCard";
 import imgSrc from './Images/pexels-photo-415829.webp'
+import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
 
 // CSS written in homepage css
 function ReviewsSection() {
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const cardContainerRef = useRef(null);
+    const [isAnimating, setIsAnimating] = useState(false);
+
+    const handlePrev = () => {
+        if (currentIndex === 0) {
+            setCurrentIndex(cards.length - 1);
+        } else {
+            setCurrentIndex(currentIndex - 1);
+        }
+        scrollToCard(currentIndex);
+    };
+
+    const handleNext = () => {
+        if (currentIndex === cards.length - 1) {
+            setCurrentIndex(0);
+        } else {
+            setCurrentIndex(currentIndex + 1);
+        }
+        scrollToCard(currentIndex);
+    };
+
+    const scrollToCard = (index) => {
+        if (cardContainerRef.current) {
+          const cardGroupWidth = cardContainerRef.current.offsetWidth;
+          cardContainerRef.current.scrollLeft = index * cardGroupWidth;
+          setIsAnimating(true);
+          setTimeout(() => {
+            setIsAnimating(false);
+          }, 500); // Adjust the duration as needed
+        }
+    };
+
+    const cards = [
+        <div key="card-group" className="card-group">
+          <ReviewCard
+            key={1}
+            review="This is a fantastic product! I highly recommend it."
+            username="John Doe"
+            profileImage={imgSrc}
+          />
+          <ReviewCard
+            key={2}
+            review="This is a fantastic product! I highly recommend it."
+            username="John Doe"
+            profileImage={imgSrc}
+          />
+          <ReviewCard
+            key={3}
+            review="This is a fantastic product! I highly recommend it."
+            username="John Doe"
+            profileImage={imgSrc}
+          />
+        </div>,
+        <div key="card-group" className="card-group">
+          <ReviewCard
+            key={4}
+            review="This is a fantastic product! I highly recommend it."
+            username="Jane Doe"
+            profileImage={imgSrc}
+          />
+          <ReviewCard
+            key={5}
+            review="This is a fantastic product! I highly recommend it."
+            username="Jane Doe"
+            profileImage={imgSrc}
+          />
+          <ReviewCard
+            key={6}
+            review="This is a fantastic product! I highly recommend it."
+            username="Jane Doe"
+            profileImage={imgSrc}
+          />
+        </div>
+        // Add more card groups here if needed
+    ];
+
     return (
         <>
             <div className="review-section-container">
                 <div className="review-tagline-div">
-                    <div className="review-tagline-innerdiv"><span className="review-tagline">Explore the testimonials from our generous community of donors and volunteers</span>
-                    {/* <img src={imgSrc1} /> */}
-                    </div>
+                    <h2 className="review-tagline">What Our Supporters Say</h2>
                 </div>
                 <div className="review-section">
-                    <div className="review-cards-container">
-                        <ReviewCard
-                            review="This is a fantastic product! I highly recommend it."
-                            username="John Doe"
-                            profileImage={imgSrc}
-                        />
-                        <ReviewCard
-                            review="This is a fantastic product! I highly recommend it."
-                            username="John Doe"
-                            profileImage={imgSrc}
-                        />
-                        <ReviewCard
-                            review="This is a fantastic product! I highly recommend it."
-                            username="JohnDoe"
-                            profileImage={imgSrc}
-                        />
-                        <ReviewCard
-                            review="This is a fantastic product! I highly recommend it."
-                            username="JohnDoe"
-                            profileImage={imgSrc}
-                        />
-                        <ReviewCard
-                            review="This is a fantastic product! I highly recommend it."
-                            username="JohnDoe"
-                            profileImage={imgSrc}
-                        />
-                        <ReviewCard
-                            review="This is a fantastic product! I highly recommend it."
-                            username="JohnDoe"
-                            profileImage={imgSrc}
-                        />
+                    <button className="scroll-btn prev" onClick={handlePrev}>
+                        <FaChevronLeft />
+                    </button>
+                    <div
+                    className={`card-groups-container ${isAnimating ? 'animate' : ''}`}
+                    ref={cardContainerRef}
+                    >
+                        {cards[currentIndex]}
                     </div>
+                    <button className="scroll-btn next" onClick={handleNext}>
+                        <FaChevronRight />
+                    </button>
                 </div>
             </div>
-            <br></br><br></br><br></br><br></br><br></br>
         </>
     )
 }
