dab45dbe4d8296db5fba905f0a9bf801357e6b6c
Bhavya Arora <bhavya56arora11@gmail.com>
2024-03-21 23:53:31 +0530

    Pop-up added

--- frontend/src/components/PartnerPage.js
+++ frontend/src/components/PartnerPage.js
@@ -1,14 +1,11 @@
 import React, { useState, useEffect, useRef } from "react";
 import "../CSS/PartnersPage.css";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 
 const PartnersPage = () => {
-
   const [brandPartners, setBrandPartners] = useState([]);
   const [orgPartners, setOrgPartners] = useState([]);
   const [loadingPercentage, setLoadingPercentage] = useState(0);
+  const [selectedOrg, setSelectedOrg] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -41,27 +38,60 @@ const PartnersPage = () => {
     return <div>Loading... {loadingPercentage}%</div>;
   }
 
+  const handleOrgClick = (org) => {
+    setSelectedOrg(org);
+  };
+
+  const closePopup = () => {
+    setSelectedOrg(null);
+  };
   return (
     <div className="partners-page">
-
-<div className="partners-section">
-  <h2 className="partners-heading">Organisation Partners</h2>
-  <div className="org-partners-container">
-      {orgPartners.map((partner, index) => (
-        <div className="partner-card" key={index}>
-          <div className="partner-card-border-top">
-            <img
-              className="img"
-              src={`https://via.placeholder.com/150`}
-              alt={partner.orgName}
-            />
-          </div>
-          <span>{partner.orgName}</span>
-          <p>{partner.motive}</p>
+      <div className="partners-section">
+        <h2 className="partners-heading">Organisation Partners</h2>
+        <div className="org-partners-container">
+          {orgPartners.map((partner, index) => (
+            <div
+              className="partner-card"
+              key={index}
+              onClick={() => handleOrgClick(partner)}
+            >
+              <div className="partner-card-border-top">
+                <img
+                  className="img"
+                  src={`https://via.placeholder.com/150`}
+                  alt={partner.orgName}
+                />
+              </div>
+              <span>{partner.orgName}</span>
+              <p>{partner.motive}</p>
+            </div>
+          ))}
         </div>
-      ))}
-  </div>
-</div>
+      </div>
+
+      {selectedOrg && (
+        <div className="popup">
+          <div className="popup-container">
+            <div className="popup-content">
+              <img
+                className="img"
+                src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg"
+                alt={selectedOrg.orgName}
+              />
+                  <h3>{selectedOrg.orgName}</h3>
+                  <p>{selectedOrg.email}</p>
+                  <p>{selectedOrg.address}</p>
+                  <p>{selectedOrg.contactNumber}</p>
+                  <p>The motive of an NGO (Non-Governmental Organization) encapsulates a multifaceted commitment to catalyzing positive change in society. Rooted in a fervent dedication to addressing pressing issues, the motive of an NGO is driven by a profound sense of social responsibility and a desire to alleviate suffering, promote human rights, and foster sustainable development. At its core, an NGO operates with the overarching goal of advocating for marginalized communities, amplifying their voices, and empowering them to access resources, opportunities, and essential services. Whether focused on humanitarian aid, environmental conservation, healthcare access, education, or socio-economic development, the motive of an NGO is guided by principles of equity, justice, and inclusivity.</p>
+                  {/* <p>{selectedOrg.motive}</p> */}
+            </div>
+            <span className="close-button" onClick={closePopup}>
+              &times;
+            </span>
+          </div>
+        </div>
+      )}
 
       <div className="partners-section">
         <h2 className="partners-heading">Brand Partners</h2>
